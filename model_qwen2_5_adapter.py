2f329bdde1e5b4224d6af5e04aef945e03b7225f
xndien2004 <xuandienk4@gmail.com>
2025-03-23 21:59:12 +0700

    update adapter_v1

--- model_qwen2_5_adapter.py
+++ model_qwen2_5_adapter.py
@@ -1,21 +1,23 @@
 from typing import Tuple
 import torch
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from qwen2_5_adapter_v1 import Qwen2AdapterV1Config, Qwen2AdapterV1ForCausalLM
 
 def Qwen2_5_Adapter(model_name: str, adapter_len: int = 64, adapter_layer: int = 4) -> Tuple[Qwen2AdapterV1ForCausalLM, AutoTokenizer]:
-    config = Qwen2AdapterV1Config.from_pretrained(model_name)
-    config.adapter_len = adapter_len
-    config.adapter_layer = adapter_layer
-    config._attn_implementation = "eager"
+    # config = Qwen2AdapterV1Config.from_pretrained(model_name)
+    # config.adapter_len = adapter_len
+    # config.adapter_layer = adapter_layer
+    # config._attn_implementation = "eager"
 
-    model = Qwen2AdapterV1ForCausalLM.from_pretrained(
-        model_name,
-        config=config,
-        trust_remote_code=True,
-        ignore_mismatched_sizes=True,
-        torch_dtype=torch.float16
-    ).to("cuda")
+    # model = Qwen2AdapterV1ForCausalLM.from_pretrained(
+    #     model_name,
+    #     config=config,
+    #     trust_remote_code=True,
+    #     ignore_mismatched_sizes=True,
+    #     torch_dtype=torch.float16
+    # ).to("cuda")
+
+    model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True).to("cuda")
 
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
